ad6a886223a8c99195d81fe34f3da69d7adba34e
unknown <tereza.raidlova@gmail.com>
2020-09-26 21:17:38 +0200

    api - vuex, plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,10 +21,10 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ["~/assets/main.scss"],
+  css: [`~/assets/main.scss`],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [`~/plugins/getfood.server.js`],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
