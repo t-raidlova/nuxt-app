73556c3e446f05d4cbd7be747b28e0b36b315fb0
unknown <tereza.raidlova@gmail.com>
2020-09-27 19:31:35 +0200

    cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,9 +1,53 @@
 <template>
-  <main class="container cart"></main>
+  <main class="container cart">
+    <section v-if="cart.length">
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Add Ons</th>
+            <th>Amount</th>
+            <th>Total Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in cart" :key="item.id">
+            <td>
+              {{ item.item }}
+              <span v-if="item.options">- {{ item.option }}</span>
+            </td>
+            <td>
+              <span v-for="addon in item.addOns" :key="addon" class="comma">{{
+                addon
+              }}</span>
+            </td>
+            <td>{{ item.count }}</td>
+            <td>{{ item.combinedPrice }} €</td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td class="total">Total: {{ totalPrice.toFixed(2) }} €</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <AppEmptyCart />
+  </main>
 </template>
 
 <script>
-export default {};
+import { mapState, mapGetters } from "vuex";
+import AppEmptyCart from "@/components/AppEmptyCart.vue";
+
+export default {
+  components: {
+    AppEmptyCart
+  },
+  computed: {
+    ...mapState(["cart"]),
+    ...mapGetters(["totalPrice"])
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
